ff145420a24b155da320bb4aec493b2d5fd2cca7
Daredevil <daredevil@rns-daredevil.local>
2018-03-27 15:55:06 +0200

    Affichage de la categorie et des tags sur accueil

--- model/database.php
+++ model/database.php
@@ -6,43 +6,45 @@ $connection = new PDO("mysql:host=" . $db_host . ";dbname=" . $db_name, $db_user
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8', lc_time_names = 'fr_FR';",
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_EMULATE_PREPARES => false
-]);
+        ]);
 
 function getAllPhotos(): array {
     global $connection;
-    
+
     $query = "SELECT
-                id,
-                titre,
-                image,
-                nb_likes,
-                date_creation,
-                DATE_FORMAT(date_creation, '%e %M %Y') AS 'date_creation_format'
+                photo.id,
+                photo.titre,
+                photo.image,
+                photo.nb_likes,
+                photo.date_creation,
+                DATE_FORMAT(photo.date_creation, '%e %M %Y') AS 'date_creation_format',
+                categorie.titre AS categorie
             FROM photo
-            ORDER BY date_creation DESC
+            INNER JOIN categorie ON categorie.id = photo.categorie_id
+            ORDER BY photo.date_creation DESC
             LIMIT 6;";
-    
+
     $stmt = $connection->prepare($query);
     $stmt->execute();
-    
+
     return $stmt->fetchAll();
 }
 
 function getPhoto(int $id): array {
     global $connection;
-    
+
     $query = "SELECT * FROM photo WHERE id = :id";
-    
+
     $stmt = $connection->prepare($query);
     $stmt->bindParam(":id", $id);
     $stmt->execute();
-    
+
     return $stmt->fetch();
 }
 
 function getAllCommentairesByPhoto(int $id): array {
     global $connection;
-    
+
     $query = "SELECT
                 id,
                 contenu,
@@ -50,25 +52,38 @@ function getAllCommentairesByPhoto(int $id): array {
             FROM commentaire
             WHERE photo_id = :id
             ORDER BY date_creation;";
-    
+
     $stmt = $connection->prepare($query);
     $stmt->bindParam(':id', $id);
     $stmt->execute();
-    
+
     return $stmt->fetchAll();
 }
 
 function insertCommentaire(string $contenu, int $photo_id) {
     global $connection;
-    
+
     $query = "INSERT INTO commentaire (contenu, date_creation, photo_id) VALUES (:contenu, NOW(), :photo_id)";
-    
+
     $stmt = $connection->prepare($query);
     $stmt->bindParam(':contenu', $contenu);
     $stmt->bindParam(':photo_id', $photo_id);
     $stmt->execute();
 }
 
+function getAllTagsByPhotos(int $id): array {
+    global $connection;
 
+    $query = "SELECT
+                tag.id,
+                tag.titre
+            FROM tag
+            INNER JOIN photo_has_tag ON photo_has_tag.tag_id = tag.id
+            WHERE photo_has_tag.photo_id = :id;";
 
+    $stmt = $connection->prepare($query);
+    $stmt->bindParam(':id', $id);
+    $stmt->execute();
 
+    return $stmt->fetchAll();
+}
